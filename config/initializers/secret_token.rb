60597776d4d83097e3e2df3727583eb34ed0d37d
Hongliang TIAN <tatetian@gmail.com>
2012-04-03 10:35:57 +0800

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeastMind::Application.config.secret_token = 'd8e44c20d8223e19ed53abdae995d595abb1af2311ab7cd5ad92a825f9305024db74bc54d1833fcbb584898ca267756111aa8bd71ee54b4933e4b480760713e2'
